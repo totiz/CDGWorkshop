d6cbb032c58f1ee108366ed32d4bd6ce731cda31
Nattapon Nimakul <totizy@gmail.com>
2018-08-01 10:50:23 +0700

    add create account

---
+++ dappbank/src/components/BankSystem.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <hr />
+    <h2>
+      Create Account
+    </h2>
+    <input type="text" v-model="userInputAccountName" placeholder="Account name"><br>
+    <button @click="createAccount">Create</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BankSystem',
+  data () {
+    return {
+      // Intial System Accounts
+      accounts: {
+        '84983390593904': {
+          name: 'Nattapon',
+          balance: 0
+        }
+      },
+
+      // Create Account
+      userInputAccountName: ''
+    }
+  },
+  methods: {
+    createAccount () {
+      console.log(this.createAccountName)
+      console.log()
+
+      let accountName = this.userInputAccountName
+      let accountNumber = this.randomAccountNumber()
+
+      this.accounts[accountNumber] = {
+        name: accountName,
+        balance: 0
+      }
+    },
+
+    randomAccountNumber () {
+      return parseInt(Math.random() * 9999999999).toString()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- dappbank/src/views/Home.vue
+++ dappbank/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BankSystem/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BankSystem from '@/components/BankSystem'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    BankSystem
   }
 }
 </script>
