2ee1877bb79b1db455be4d6d5aef884d79430031
Artit Kiuwilai <first087@gmail.com>
2018-08-01 14:44:43 +0700

    Update docker command for remove old container

--- DAppKatinrun/runDocker.sh
+++ DAppKatinrun/runDocker.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -x
-docker stop katinrun
-docker rm katinrun
+docker rm -f katinrun
 docker run --name katinrun -p 8080:80 -v $PWD/:/usr/share/nginx/html:ro -d nginx:alpine
 set +x
 echo "Go to http://localhost:8080"
