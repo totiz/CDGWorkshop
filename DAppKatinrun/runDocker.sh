aa1487eefc1f3c1f4b8bb144bbd327bb87ad878d
Artit Kiuwilai <first087@gmail.com>
2018-08-01 14:40:47 +0700

    Change nginx docker image to apline version

--- DAppKatinrun/runDocker.sh
+++ DAppKatinrun/runDocker.sh
@@ -2,6 +2,6 @@
 set -x
 docker stop katinrun
 docker rm katinrun
-docker run --name katinrun -p 8080:80 -v $PWD/:/usr/share/nginx/html:ro -d nginx
+docker run --name katinrun -p 8080:80 -v $PWD/:/usr/share/nginx/html:ro -d nginx:alpine
 set +x
 echo "Go to http://localhost:8080"
